e3793d7feb7a433409f2cd446038e96f168ee174
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-07-16 20:41:12 +0530

    Move Zeros

---
+++ MoveZeros.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+
+        for (int current = 0; current < nums.length; current++) {
+            if (nums[current] != 0) {
+                int temp = nums[current];
+                nums[current] = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int nums[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
